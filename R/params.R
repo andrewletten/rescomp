9b2c82bcb762f18f0c3a510d3b67198131e4ec26
Christopher Brown <pyrrhicpachyderm@gmail.com>
2024-03-22 09:40:30 +1000

    Add rescomp_param_list().

--- R/params.R
+++ R/params.R
@@ -1,3 +1,32 @@
+#' Create a list of named rescomp parameters
+#'
+#' Produces an object suitable to pass as the `params` to `spec_rescomp`.
+#'
+#' @param ... Named arguments used to create the params list.
+#'
+#' @details
+#' Must be given zero or more named arguments, which are used to build the params list.
+#' `rescomp_param` objects are parsed to allow time dependence, while other objects are passed into the list directly.
+#'
+#' @returns S3 object of class `rescomp_param_list`.
+#' @export
+#'
+#' @examples
+#' params <- rescomp_param_list(
+#'   r = 0.2,
+#'   s = c(3, 4),
+#'   conc = rescomp_param_custom(function(t) {
+#'     t^2
+#'   })
+#' )
+#' get_params(params, 0.5)
+#' get_params(params, 2)
+rescomp_param_list <- function(...) {
+  param_list <- list(...)
+  class(param_list) <- c("rescomp_param_list", "rescomp_param")
+  return(param_list)
+}
+
 #' Create a rescomp parameter using an arbitrary function
 #'
 #' Produces an object suitable to include in a `rescomp_param_list`.
@@ -39,8 +68,23 @@ rescomp_param_custom <- function(func) {
 #' get_params(antibiotic_conc, 0)
 #' get_params(antibiotic_conc, 0.5)
 #' get_params(antibiotic_conc, 1)
+#'
+#' params <- rescomp_param_list(r = 0.2, antibiotic_conc = antibiotic_conc)
+#' get_params(params, 0.5)
 get_params <- function(param_obj, t) {
   UseMethod("get_params")
+}
+
+#' @export
+get_params.rescomp_param_list <- function(param_obj, t) {
+  param_list <- param_obj
+  class(param_list) <- "list"
+  for (name in names(param_list)) {
+    if ("rescomp_param" %in% class(param_list[[name]])) {
+      param_list[[name]] <- get_params(param_list[[name]], t)
+    }
+  }
+  return(param_list)
 }
 
 #' @export
