c247f88e98caac42684726209a26a0bd6c8fc402
Christopher Brown <pyrrhicpachyderm@gmail.com>
2024-11-12 11:37:03 +1000

    Add a periodic event schedule.

--- R/event_schedule.R
+++ R/event_schedule.R
@@ -17,6 +17,26 @@ spec_event_schedule_fixed <- function(event_obj, times, priority = 0) {
   return(schedule)
 }
 
+#' Define a schedule of an event occurring repeatedly at a fixed period
+#'
+#' Produces an event schedule object suitable for passing in the `events` argument to `spec_rescomp()`.
+#'
+#' @param event_obj An object of class `rescomp_event`.
+#' @param period A number: the period at which the event occurs.
+#' @param start_time A number: the time at which the event first occurs.
+#' @param priority A number. If two events occur at the same time, the event with the lower priority number is processed first.
+#'
+#' @returns S3 object of class `rescomp_event_schedule`.
+#' @export
+#'
+#' @examples
+#' # TODO
+spec_event_schedule_periodic <- function(event_obj, period, start_time = period, priority = 0) {
+  schedule <- list(event_obj = event_obj, period = period, start_time = start_time, priority = priority)
+  class(schedule) <- c("rescomp_event_schedule_periodic", "rescomp_event_schedule")
+  return(schedule)
+}
+
 #' Gets the list of times from a `rescomp_event_schedule` object
 #'
 #' This function is normally only for internal use, but is exported to aid users in debugging their created `rescomp_event` objects.
@@ -36,4 +56,9 @@ get_event_times <- function(event_schedule_obj, totaltime) {
 #' @export
 get_event_times.rescomp_event_schedule_fixed <- function(event_schedule_obj, totaltime) {
   return(event_schedule_obj$times)
+}
+
+#' @export
+get_event_times.rescomp_event_schedule_periodic <- function(event_schedule_obj, totaltime) {
+  return(seq(from = event_schedule_obj$start_time, to = totaltime, by = event_schedule_obj$period))
 }
