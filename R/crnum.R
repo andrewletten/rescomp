dbb6c8bfa5dde2b53ac7732ce4c6dc1d2acc7353
Christopher Brown <pyrrhicpachyderm@gmail.com>
2025-04-30 19:18:52 +1000

    Add dimension checking and recycling to propagate functions on vectors/matrices.

--- R/crnum.R
+++ R/crnum.R
@@ -1,3 +1,58 @@
+#' Check the dimensions of a vector/matrix match expected spnum and resnum.
+#'
+#' @param vec A numeric vector, which is expected to be of length spnum/resnum, or of length 1, in which case it is recycled.
+#' @param mat A numeric matrix, which is expected to have spnum rows and resnum columns.
+#' @param spnum,resnum Numeric vectors of length 1: the expected number of rows and columns of the given object.
+#'
+#' @returns Returns vec or mat, recycled to the appropriate dimensions if necessary.
+#' @noRd
+#'
+#' @examples
+#' # TODO
+enforce_crnum <- function(mat, spnum, resnum, call = rlang::caller_env()) {
+  if (nrow(mat) != spnum || ncol(mat) != resnum) {
+    cli::cli_abort(c(
+      "Matrix dimensions do not match spnum and resnum.",
+      "x" = "Matrix had {nrow(mat)} row{?s} and {ncol(mat)} columns{?s}.",
+      "i" = "Expected {spnum} (spnum) row{?s} and {resnum} (resnum) columns{?s}."
+    ), call = call)
+  } else {
+    return(mat)
+  }
+}
+
+#' @rdname enforce_crnum
+#' @noRd
+enforce_cnum <- function(vec, spnum, call = rlang::caller_env()) {
+  if (length(vec) == 1) {
+    return(rep(vec, times = spnum))
+  } else if (length(vec) != spnum) {
+    cli::cli_abort(c(
+      "Vector length does not match spnum.",
+      "x" = "Vector had length {length(vec)}.",
+      "i" = "Expected length {spnum} (spnum)."
+    ), call = call)
+  } else {
+    return(vec)
+  }
+}
+
+#' @rdname enforce_crnum
+#' @noRd
+enforce_rnum <- function(vec, resnum, call = rlang::caller_env()) {
+  if (length(vec) == 1) {
+    return(rep(vec, times = resnum))
+  } else if (length(vec) != resnum) {
+    cli::cli_abort(c(
+      "Vector length does not match resnum.",
+      "x" = "Vector had length {length(vec)}.",
+      "i" = "Expected length {resnum} (resnum)."
+    ), call = call)
+  } else {
+    return(vec)
+  }
+}
+
 #' Propagate `spnum` and `resnum` down from `spec_rescomp()` to objects passed to it.
 #'
 #' @param obj An object to be adjusted to the correct `spnum` and `resnum`.
@@ -17,6 +72,10 @@ propagate_crnum.default <- function(obj, spnum, resnum) {
   return(obj)
 }
 
+propagate_crnum.matrix <- function(obj, spnum, resnum) {
+  return(enforce_crnum(obj, spnum, resnum))
+}
+
 #' @rdname propagate_crnum
 #' @noRd
 propagate_cnum <- function(obj, spnum) {
@@ -27,6 +86,14 @@ propagate_cnum.default <- function(obj, spnum) {
   return(obj)
 }
 
+propagate_cnum.numeric <- function(obj, spnum) {
+  return(enforce_cnum(obj, spnum))
+}
+
+propagate_cnum.integer <- function(obj, spnum) {
+  return(enforce_cnum(obj, spnum))
+}
+
 #' @rdname propagate_crnum
 #' @noRd
 propagate_rnum <- function(obj, resnum) {
@@ -35,4 +102,12 @@ propagate_rnum <- function(obj, resnum) {
 
 propagate_rnum.default <- function(obj, resnum) {
   return(obj)
+}
+
+propagate_rnum.numeric <- function(obj, resnum) {
+  return(enforce_rnum(obj, resnum))
+}
+
+propagate_rnum.integer <- function(obj, resnum) {
+  return(enforce_rnum(obj, resnum))
 }
